e6f48b1c204e1d70f08c0b4c02a86c6bac809b8f
Anton <chmutoff@yandex.ru>
2019-02-02 18:08:35 +0100

    Improved SN74HC595 examples
    + There are two examples, first with a single SN74HC595 and second with two daisy chained SN74HC595

--- examples/sn74hc595-daisy.go
+++ examples/sn74hc595-daisy.go
@@ -7,14 +7,14 @@ import (
 )
 
 func main() {
-	sreg, err := shiftregister.NewSn74hc595(17, 27, 18, 8, false)
+	sreg, err := shiftregister.NewSn74hc595(17, 27, 18, 16, false)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	defer sreg.Close()
 
-	leds := []uint{0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}
+	leds := []uint{0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000}
 	for {
 		for _, led := range leds {
 			sreg.Write(led)
