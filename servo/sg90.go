b12ce149577eb1bfd34553385508f1e1df8ba04d
Anton <chmutoff@yandex.ru>
2019-03-05 23:58:54 +0100

    SG90 refactor

--- servo/sg90.go
+++ servo/sg90.go
@@ -6,44 +6,44 @@ import (
 
 const cycleLength = 400
 
-type Sg90 struct {
+type sg90 struct {
 	pin rpio.Pin
 }
 
-func NewSg90(pin int) (*Sg90, error) {
+func NewSg90(pin int) (*sg90, error) {
 	if err := rpio.Open(); err != nil {
 		return nil, err
 	}
 
-	servo := Sg90{}
+	servo := sg90{}
 	servo.initPin(pin)
 
 	return &servo, nil
 }
 
-func (servo *Sg90) initPin(pin int) {
+func (servo *sg90) initPin(pin int) {
 	servo.pin = rpio.Pin(pin)
 	servo.pin.Pwm()
 	servo.pin.Freq(20000) //50 * cycleLength
 }
 
-func (servo Sg90) InitialPosition() {
+func (servo sg90) InitialPosition() {
 	servo.pin.DutyCycle(10, cycleLength)
 }
 
-func (servo Sg90) NeutralPosition() {
+func (servo sg90) NeutralPosition() {
 	servo.pin.DutyCycle(30, cycleLength)
 }
 
-func (servo Sg90) FinalPosition() {
+func (servo sg90) FinalPosition() {
 	servo.pin.DutyCycle(50, cycleLength)
 }
 
-func (servo Sg90) Turn(degree int) {
+func (servo sg90) Turn(degree int) {
 	y := (4.0 / 18.0 * float64(degree)) + 10.0
 	servo.pin.DutyCycle(uint32(y), cycleLength)
 }
 
-func (Sg90) Close() error {
+func (sg90) Close() error {
 	return rpio.Close()
 }
