fb8fca595ab760ba00a441a7f8e46775ee806b30
Anton <chmutoff@yandex.ru>
2019-06-24 16:10:20 +0200

    TB6612FNG Added some code to set the speed up.

--- dcmotordriver/tb6612fng.go
+++ dcmotordriver/tb6612fng.go
@@ -6,12 +6,16 @@ type tb6612fng struct {
 	stby rpio.Pin
 
 	pwmA rpio.Pin
-	aIn1 rpio.Pin
-	aIn2 rpio.Pin
+	inA1 rpio.Pin
+	inA2 rpio.Pin
+	minA uint8
+	maxA uint8
 
 	pwmB rpio.Pin
-	bIn1 rpio.Pin
-	bIn2 rpio.Pin
+	inB1 rpio.Pin
+	inB2 rpio.Pin
+	minB uint8
+	maxB uint8
 }
 
 type MotorDirection uint8
@@ -37,25 +41,25 @@ func NewTb6612fng(stby rpio.Pin, pwmA rpio.Pin, aIn1 rpio.Pin, aIn2 rpio.Pin, pw
 	driver.pwmA.Pwm()
 	driver.pwmA.Freq(1920)
 
-	driver.aIn1 = rpio.Pin(aIn1)
-	driver.aIn1.Output()
-	driver.aIn1.Low()
+	driver.inA1 = rpio.Pin(aIn1)
+	driver.inA1.Output()
+	driver.inA1.Low()
 
-	driver.aIn2 = rpio.Pin(aIn2)
-	driver.aIn2.Output()
-	driver.aIn2.Low()
+	driver.inA2 = rpio.Pin(aIn2)
+	driver.inA2.Output()
+	driver.inA2.Low()
 
 	driver.pwmB = rpio.Pin(pwmB)
 	driver.pwmB.Pwm()
 	driver.pwmB.Freq(1920)
 
-	driver.bIn1 = rpio.Pin(bIn1)
-	driver.bIn1.Output()
-	driver.bIn1.Low()
+	driver.inB1 = rpio.Pin(bIn1)
+	driver.inB1.Output()
+	driver.inB1.Low()
 
-	driver.bIn2 = rpio.Pin(bIn2)
-	driver.bIn2.Output()
-	driver.bIn2.Low()
+	driver.inB2 = rpio.Pin(bIn2)
+	driver.inB2.Output()
+	driver.inB2.Low()
 
 	driver.stby.High() // Enable board
 
@@ -76,11 +80,11 @@ func (driver tb6612fng) MotorACounterclockwise() {
 
 func (driver tb6612fng) MotorASetDirection(direction MotorDirection) {
 	if direction == Clockwise {
-		driver.aIn1.High()
-		driver.aIn2.Low()
+		driver.inA1.High()
+		driver.inA2.Low()
 	} else {
-		driver.aIn1.Low()
-		driver.aIn2.High()
+		driver.inA1.Low()
+		driver.inA2.High()
 	}
 }
 
@@ -98,12 +102,20 @@ func (driver tb6612fng) MotorBCounterclockwise() {
 
 func (driver tb6612fng) MotorBSetDirection(direction MotorDirection) {
 	if direction == Clockwise {
-		driver.bIn1.High()
-		driver.bIn2.Low()
+		driver.inB1.High()
+		driver.inB2.Low()
 	} else {
-		driver.bIn1.Low()
-		driver.bIn2.High()
+		driver.inB1.Low()
+		driver.inB2.High()
 	}
+}
+
+func (driver tb6612fng) SetMinMaxMotorASpeed(min, max uint8) {
+
+}
+
+func (driver tb6612fng) SetMinMaxMotorBSpeed(min, max uint8) {
+
 }
 
 func (driver tb6612fng) Close() error {
